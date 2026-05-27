6eeee297be1275330f852b327a800cf6d866af0d
Himanshu Sable <himanshusable2488@gmail.com>
2026-05-27 12:14:11 +0530

    modified listen function

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,13 @@ const connectToDB = require("./src/config/db.js")
 
 connectToDB();
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
+
+
+
+   
